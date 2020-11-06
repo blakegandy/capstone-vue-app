7c33600fadfff7c5159775874b8f64787e064d78
blakegandy <gandyb98@gmail.com>
2020-11-06 11:09:46 -0500

    first commit to github including signup and login functionality

---
+++ src/views/Signup.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="signup">
+    <form v-on:submit.prevent="submit()">
+      <h1>Signup</h1>
+      <ul>
+        <li class="text-danger" v-for="error in errors">{{ error }}</li>
+      </ul>
+      <div class="form-group">
+        <label>First Name:</label>
+        <input type="text" class="form-control" v-model="firstName" />
+      </div>
+      <div class="form-group">
+        <label>Last Name:</label>
+        <input type="text" class="form-control" v-model="lastName" />
+      </div>
+      <div class="form-group">
+        <label>Phone Number:</label>
+        <input type="text" class="form-control" v-model="phoneNumber" />
+      </div>
+      <div class="form-group">
+        <label>Email:</label>
+        <input type="email" class="form-control" v-model="email" />
+      </div>
+      <div class="form-group">
+        <label>Password:</label>
+        <input type="password" class="form-control" v-model="password" />
+      </div>
+      <div class="form-group">
+        <label>Password confirmation:</label>
+        <input
+          type="password"
+          class="form-control"
+          v-model="passwordConfirmation"
+        />
+      </div>
+      <div class="form-group">
+        <label>Are you a Stylist?:</label>
+        <input type="checkbox" class="form-control" v-model="stylist" />
+      </div>
+      <div class="form-group">
+        <label>Salon:</label>
+        <input type="text" class="form-control" v-model="salon" />
+      </div>
+      <div class="form-group">
+        <label>Specialty:</label>
+        <input type="text" class="form-control" v-model="specialty" />
+      </div>
+      <input type="submit" class="btn btn-primary" value="Submit" />
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function() {
+    return {
+      firstName: "",
+      lastName: "",
+      phoneNumber: "",
+      email: "",
+      password: "",
+      passwordConfirmation: "",
+      stylist: Boolean,
+      salon: "",
+      specialty: "",
+      errors: [],
+    };
+  },
+  methods: {
+    submit: function() {
+      var params = {
+        firstName: this.firstName,
+        lastName: this.lastName,
+        phoneNumber: this.phoneNumber,
+        email: this.email,
+        password: this.password,
+        password_confirmation: this.passwordConfirmation,
+        stylist: this.stylist,
+        salon: this.salon,
+        specialty: this.specialty,
+      };
+      axios
+        .post("/api/users", params)
+        .then((response) => {
+          this.$router.push("/login");
+        })
+        .catch((error) => {
+          this.errors = error.response.data.errors;
+        });
+    },
+  },
+};
+</script>
